9b21a267544f028ef9b16bef322ac25f0527ba32
BorisAntares <145720511+BorisAntares@users.noreply.github.com>
2023-09-21 18:32:01 -0300

    Create AntaresHub.lua
    Sla

---
+++ AntaresHub.lua
@@ -0,0 +1,61 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Antares Hub V0.2", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", introText = Antares Hub})
+OrionLib:MakeNotification({
+	Name = "Maded By Boris",
+	Content = "Pra poder colaborar com o hub, por favor entre no nosso discoard: .gg/uAtpbJG4y4",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+local Tab = Window:MakeTab({
+	Name = "Kit Brookhaven",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "Hubs"
+})
+Tab:AddButton({
+	Name = "Ice Hub",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"))()
+Tava:AddButton({
+Name = "Unfair Hub ⚠️ TALVEZ NÃO FUNCIONE NO MOBILE⚠️",
+	Callback = function()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/rblxscriptsnet/unfair/main/rblxhub.lua'),true))()
+
+      		print("button pressed")
+  	end    
+})
+local Section = Tab:AddSection({
+	Name = "Guis"
+})
+
+Tab:AddButton({
+	Name = "Infinite Yield",
+	Callback = function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+
+Tab:AddButton({
+	Name = "Fly Gui",
+	Callback = function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro69Yes/sussy-Script/main/SecuredFlyGuiv3.lua'))()
+	
+
+      		print("button pressed")
+  	end    
+})
+local Tab = Window:MakeTab({
+	Name = "Créditos",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "feito pelo Lukas Silva/Boris/robloxboris, obrigado pelo antares pra me incentivar a fazer isso",
+	Callback = function()
+
+      		print("button pressed")
+
+OrionLib:Init()
+                  
