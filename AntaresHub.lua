657de503d64c005a5c1a4d43980ffcb879fbcf5f
BorisAntares <145720511+BorisAntares@users.noreply.github.com>
2023-09-21 19:04:23 -0300

    Update AntaresHub.lua
    Desbugando

--- AntaresHub.lua
+++ AntaresHub.lua
@@ -1,3 +1,4 @@
+--
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Antares Hub V0.2", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", introText = Antares Hub})
 OrionLib:MakeNotification({
