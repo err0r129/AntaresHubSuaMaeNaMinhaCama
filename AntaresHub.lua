fe380c809cbcebab1dd1e99aac3aaf345bcdcc95
BorisLua <145720511+BorisLua@users.noreply.github.com>
2023-12-28 19:44:01 -0300

    tentativa

--- AntaresHub.lua
+++ AntaresHub.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Antares Hub V0.4.1", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", introText = eae})
 OrionLib:MakeNotification({
 	Name = "Maded By Boris",
