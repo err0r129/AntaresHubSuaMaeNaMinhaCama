c15b33d30b48529502d9427736587f8b4fed66ef
BorisLua <145720511+BorisLua@users.noreply.github.com>
2024-01-05 06:10:18 -0300

    Atualizar o EnglishAntaresHub.lua

--- EnglishAntaresHub.lua
+++ EnglishAntaresHub.lua
@@ -1,4 +1,3 @@
-
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Antares Hub English Ver. V0.4.0", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", introText = eae})
 OrionLib:MakeNotification({
